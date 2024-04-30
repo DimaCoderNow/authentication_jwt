0ca6757cd50dc43cfd7246f852da44c808191057
Dmitriy Zamarekhin <zam1986@yandex.ru>
2024-04-30 15:14:35 +0200

    added users in admin

--- web_site/jwt_auth/admin.py
+++ web_site/jwt_auth/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import User
+
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('username', 'email')
+    readonly_fields = ('is_active', 'is_staff')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+
+        return False
+
+
